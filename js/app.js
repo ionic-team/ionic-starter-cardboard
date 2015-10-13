dcfe24d49155ab1bf5b882265360d421fe2ecc6f
Max Lynch <max@drifty.com>
2015-10-13 11:01:37 -0500

    hacking locally

--- js/app.js
+++ js/app.js
@@ -8,12 +8,46 @@ angular.module('starter', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
       StatusBar.styleDefault();
     }
   });
 })
+
+.controller('AppCtrl', ['$scope', function($scope) {
+  var self = this;
+
+  this.getTransition = function() {
+  }
+
+  this.getPanStyle = function() {
+
+    var image = new Image();
+
+    image.onload = function () {
+      this.imageWidth = image.width;
+    };
+
+    image.src = "img/panorama.jpg";
+
+    return {
+      height: window.innerHeight + "px",
+      backgroundPosition: "-" + this.picturePosition + 'px 490px',
+      backgroundImage: "url(" + image.src + ")",
+      backgroundSize: this.imageWidth + "px 774px",
+      transition: this.getTransition(),
+      transform: "translate3d(0, 0, 0)"
+    };
+  }
+}])
